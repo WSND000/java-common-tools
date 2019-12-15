cc54ee74b8afc440933b93f594fe633388d0c9f8
WSND000 <PingXingle2019@163.com>
2019-12-15 22:16:10 +0800

    demo

--- src/main/java/com/zeroten/common/util/CheckUtils.java
+++ src/main/java/com/zeroten/common/util/CheckUtils.java
@@ -13,4 +13,32 @@ public class CheckUtils {
         }
         return flag;
     }
+
+    public static boolean isEmpty(Object[] arr) {
+        boolean flag = false;
+            if (arr==null||arr.length==0) {
+                flag = true;
+            }
+        return flag;
+    }
+
+    public static boolean equals(String str1, String str2) {
+        boolean flag = false;
+        if (str1==str2){
+            flag = true;
+        }else if(str1==null||str2==null){
+            flag = false;
+        }
+        return flag;
+    }
+    public static boolean equals2(Integer n1, Integer n2) {
+        boolean flag = false;
+        if (n1.equals(n2)){
+            flag = true;
+        }else if(n1==null||n2==null){
+            flag = false;
+        }
+        return flag;
+    }
+
 }
--- src/test/java/testCheck/TestCheck.java
+++ src/test/java/testCheck/TestCheck.java
@@ -7,11 +7,43 @@ import org.junit.Test;
 public class TestCheck {
 
     @Test
-    public void testIsEmpty() {
+    public void testIsAnyEmpty() {
         Assert.assertEquals(false, CheckUtils.isAnyEmpty(null,"a","s"));
         Assert.assertEquals(false, CheckUtils.isAnyEmpty(null,null,"s",null,"d","f"));
         Assert.assertEquals(true, CheckUtils.isAnyEmpty("a","s"));
     }
 
+    @Test
+    public void testIsEmpty() {
+        Object[] arr1 = null;
+        Object[] arr2 = new Object[4];
+        Object[] arr3 = {1,"a","2",4,"sadasd"};
+        Assert.assertEquals(true,CheckUtils.isEmpty(arr1));
+        Assert.assertEquals(false,CheckUtils.isEmpty(arr2));
+        Assert.assertEquals(false,CheckUtils.isEmpty(arr3));
+    }
 
+    @Test
+    public void testEquals() {
+        String str1 = "hello";
+        String str2 = new String("hello");
+        Assert.assertEquals(true,CheckUtils.equals("hello","hello"));
+        Assert.assertEquals(false,CheckUtils.equals(null,"hello"));
+        Assert.assertEquals(true,CheckUtils.equals(null,null));
+        Assert.assertEquals(true,CheckUtils.equals(str1,"hello"));
+        Assert.assertEquals(false,CheckUtils.equals(str2,"hello"));
+        Assert.assertEquals(false,CheckUtils.equals(str1,str2));
+    }
+
+    @Test
+    public void testEquals2() {
+        Integer n1 = 1;
+        Integer n2 = new Integer(1);
+        Integer n3 = null;
+        Assert.assertEquals(false,CheckUtils.equals2(1,2));
+        Assert.assertEquals(false,CheckUtils.equals2(1,n3));
+        Assert.assertEquals(true,CheckUtils.equals2(n1,1));
+        Assert.assertEquals(true,CheckUtils.equals2(n2,1));
+        Assert.assertEquals(true,CheckUtils.equals2(n1,n2));
+    }
 }
